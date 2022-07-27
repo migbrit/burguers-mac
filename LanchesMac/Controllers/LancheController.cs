b91760c4a4f294c8cb005a626d7503661f54f26f
Miguel Brito <miguelfelipe_steam@outlook.com>
2022-07-27 18:58:52 -0300

    Implementing the view to display the burgers

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LanchesMac.Repositories.Interfaces;
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
